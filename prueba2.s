80d260bae26906dbdee1b5a3f686dbff51047a0e
Antonio Lechuga <antoniolechuga@MacBook-Pro-de-JALR.local>
2020-03-26 15:16:56 -0600

    Modify test to store data/code at 100/200 respect.

---
+++ prueba2.s
@@ -0,0 +1,20 @@
+; Arquitectura de computadoras
+; Programa de demostración. 
+.data
+.org 100
+i:	.word32 0
+j:	.word32 0
+.text
+.org 200
+	daddi R2, R0, 0;
+	daddi r3, R0, 0;
+	daddi r5, R0, 10 ;
+WHIL:	slt R6, R2, R5
+	beqz R6, ENDW
+	daddi r3, r3, 5
+	sw R3, j(r0)
+	daddi r2, r2, 1
+	sw r2, i(r0)
+	j WHIL
+ENDW:	nop
+	halt
