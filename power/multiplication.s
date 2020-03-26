118cbbbc81c7192eee44c060133718ca9c193006
Victor Cruz <vicdan215@gmail.com>
2020-03-25 19:45:00 -0600

    Sum instead of mul for getting index

--- power/multiplication.s
+++ power/multiplication.s
@@ -8,10 +8,9 @@ output: .word32 0:10
   daddi r2, r0, 0; contador i
   daddi r5, r0, 10; limite de i
   daddi r8, r0, 2; constante de mult
-  daddi r9, r0, 8; constante de size
   daddi r10, r0, 0; i * size
   WHIL:	slt r6, r2, r5
-    dmul r10, r2, r9
+    daddi r10, r10, 8; constante de size
     beqz r6, ENDW ; brincar si i == 10
     daddi r3, r0, 0; contador j
     daddi r4, r0, 1 ; contador res
--- power/shift.s
+++ power/shift.s
@@ -7,10 +7,9 @@ output: .word32 0:10
   daddi r2, r0, 0; contador i
   daddi r5, r0, 10; limite de i
   daddi r8, r0, 1; constante para shl
-  daddi r9, r0, 8; constante de size
   daddi r10, r0, 0; i * size
   WHIL:	slt r6, r2, r5
-    dmul r10, r2, r9
+    daddi r10, r10, 8; constante de size
     beqz r6, ENDW ; brincar si i == 10
     dsllv r4, r8, r2 ; shl(r4)
     daddi r2, r2, 1 ; i++
