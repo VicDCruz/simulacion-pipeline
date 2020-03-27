148178ce8cd6d8c6171e3ab06f7649500763eb4d
Antonio Lechuga <antoniolechuga@MacBook-Pro-de-JALR.local>
2020-03-27 14:14:17 -0600

    Fix address jump

--- power/multiplication.s
+++ power/multiplication.s
@@ -1,15 +1,16 @@
 ; Arquitectura de computadoras
 ; Programa del ejercicio 2. 
 .data
-i:	    .word32 0
+i:	.word32 0
 j:      .word32 0
-output: .word32 0:9
+output: .word32 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 .text
         daddi r2, r0, 0     ; contador i
         daddi r5, r0, 10    ; limite de i
         daddi r8, r0, 2     ; constante de mult
-        daddi r10, r0, 0      ; i * size
-WHIL:	  slt r6, r2, r5
+        daddi r10, r0, -4   ; i * size
+WHIL:	slt r6, r2, r5
+        daddi r10, r10, 4   ; constante de size
         beqz r6, ENDW       ; brincar si i == 10
         daddi r3, r0, 0     ; contador j
         daddi r4, r0, 1     ; contador res
@@ -22,7 +23,6 @@ ENDW1:  daddi r2, r2, 1     ; i++
         sw r2, i(r0)        ; store 32-bit word
         sw r3, j(r0)        ; store 32-bit word
         sw r4, output(r10)  ; output[i] = res
-        daddi r10, r10, 8   ; constante de size
         j WHIL
-ENDW:	  nop
+ENDW:	nop
         halt
--- power/shift.s
+++ power/shift.s
@@ -2,19 +2,19 @@
 ; Programa del ejercicio 2. 
 .data
 i:      .word32 0
-output: .word32 0:9
+output: .word32 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 .text
         daddi r2, r0, 0     ; contador i
         daddi r5, r0, 10    ; limite de i
         daddi r8, r0, 1     ; constante para shl
-        daddi r10, r0, 0    ; i * size
+        daddi r10, r0, -4   ; i * size
 WHIL:	slt r6, r2, r5
+        daddi r10, r10, 4   ; constante de size
         beqz r6, ENDW       ; brincar si i == 10
         dsllv r4, r8, r2    ; shl(r4)
         daddi r2, r2, 1     ; i++
         sw r2, i(r0)        ; store 32-bit word
         sw r4, output(r10)  ; output[i] = res
-        daddi r10, r10, 8   ; constante de size
         j WHIL
 ENDW:	nop
         halt
