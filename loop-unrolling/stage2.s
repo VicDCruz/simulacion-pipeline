7b0afabcc68bc64a4eaca9bd32032379f53f1c16
Antonio Lechuga <antoniolechuga@MacBook-Pro-de-JALR.local>
2020-03-27 19:39:30 -0600

    Fix array size

--- loop-unrolling/stage2.s
+++ loop-unrolling/stage2.s
@@ -1,26 +1,27 @@
 .data
-i:	    .word32   32
-output: .word32   100, 78, 63, 88, 52, 91, 75, 78, 10
+i:      .word32   28
+output: .word32   100, 78, 63, 88, 52, 91, 75, 78
 .text
-        lw r1, i(r0)          ; contador i
-LOOP:   lw r10, output(r1)         ; Leer elemento vector
-        daddi r10, r10, 4     ; Sumar 4 al elemento
-        sw r10, output(r1)         ; Escribir nuevo valor
+        lw r1, i(r0)            ; contador i
+        daddi r14, r14, -4      ; valor de terminación
+LOOP:   lw r10, output(r1)      ; Leer elemento vector
+        daddi r10, r10, 4       ; Sumar 4 al elemento
+        sw r10, output(r1)      ; Escribir nuevo valor
 
-        lw r11, output-4(r1)        ; 2ª copia
+        lw r11, output-4(r1)    ; 2ª copia
         daddi r11, r11, 4
         sw r11, output-4(r1)
 
-        lw r12, output-8(r1)        ; 3a copia
+        lw r12, output-8(r1)    ; 3a copia
         daddi r12, r12, 4
         sw r12, output-8(r1)
 
-        lw r13, output-12(r1)       ; 4ª copia
+        lw r13, output-12(r1)   ; 4ª copia
         daddi r13, r13, 4
         sw r13, output-12(r1)
 
-        daddi r1, r1, -16     ; Actualizar índice
-        bne r1, r0, LOOP      ; Fin de vector?
-ENDW:	  sw r1, i(r0)          ; guardar último valor de i
+        daddi r1, r1, -16       ; Actualizar índice
+        bne r1, r14, LOOP       ; Fin de vector?
+ENDW:	sw r1, i(r0)            ; guardar último valor de i
         nop
         halt
